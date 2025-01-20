5efc956621b3cba6aa73beba04a3e96484f9fbd0
Elaine Fang <efang@lcs.on.ca>
2025-01-19 20:49:38 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by Fang Elaine on 2025/1/19.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArithmeticBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArithmeticBuddy
+//
+//  Created by Fang Elaine on 2025/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
